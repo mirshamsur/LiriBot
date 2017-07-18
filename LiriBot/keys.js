0ed2c83403a7144d79be3d4c818f8441366df8ee
Shamsur Mir <shamsurmir@gmail.com>
2017-07-17 23:09:09 -0400

    first commit

---
+++ LiriBot/keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<YpZVKyDcNq35QODuchtDTadgW>',
+  consumer_secret: '<sJh27AfIGcVZwHFJs646qhmVBnyyavxUFLyFl9vhcU8CpMs7aj>',
+  access_token_key: '<876097961134096385-inBlwgt3GJCrcqpCa5NcrzWUXmzQCHN>',
+  access_token_secret: '<x9HNm2U7L79qJx1rsPSh3UQZOBZdZi8IaJBy68DwObJjl>',
+}
+
+// Spotify Client ID and Client Secret:
+
+// Client ID
+// 1ca8134dde254f7f91d9cc4e27ce361e
+// Client Secret
+// 95bfeed8050346848856b34653b8f61f
+
+
+
